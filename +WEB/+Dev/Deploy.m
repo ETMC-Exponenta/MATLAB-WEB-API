d2ea1a5dd74e42ca576fd1b3ab9755812c56258d
Pavel Roslovets <roslovets@exponenta.ru>
2018-09-13 14:47:18 +0300

    Deploy: final message

--- +WEB/+Dev/Deploy.m
+++ +WEB/+Dev/Deploy.m
@@ -4,5 +4,8 @@ pname = 'MATLAB.WEB.API.prj';
 fname = 'MATLAB.WEB.API';
 if nargin > 0
     matlab.addons.toolbox.toolboxVersion(pname, v);
+else
+    v = matlab.addons.toolbox.toolboxVersion(pname);
 end
 matlab.addons.toolbox.packageToolbox(pname, fname);
+fprintf('MATLAB WEB API v%s has been deployed\n', v);
