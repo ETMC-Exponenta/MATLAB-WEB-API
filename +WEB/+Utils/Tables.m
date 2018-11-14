75e24a09a1d1e887d4ea842f58711d21075ae7fa
Pavel Roslovets <roslovets@exponenta.ru>
2018-11-14 09:47:09 +0300

    Tables: new great functionality

--- +WEB/+Utils/Tables.m
+++ +WEB/+Utils/Tables.m
@@ -8,27 +8,36 @@ classdef Tables
         
         function ss = concat(obj, ss, preserveFormat)
             %% Fix VK Response
+            ist = false;
             if iscell(ss)
                 ss = ss(:);
-                ist = istable(ss{1});
-                if ist
-                    ss = cellfun(@(x) table2struct(x), ss, 'un', 0);
+                emp = cellfun('isempty', ss);
+                if all(emp)
+                    ss = {};
+                elseif any(emp)
+                    ss = ss(~emp);
                 end
-                fs = cellfun(@(x) fieldnames(x), ss, 'un', 0);
-                fs = reshape(vertcat(fs{:}), [], 1);
-                fs = unique(fs);
-                ss = obj.fill_empty(ss, fs);
-                ss = vertcat(ss{:});
-                if ist || nargin < 3 || ~preserveFormat
-                    ss = struct2table(ss);
+                if ~isempty(ss)
+                    ist = cellfun(@istable, ss);
+                    ss(ist) = cellfun(@(x) table2struct(x), ss(ist), 'un', 0);
+                    fs = cellfun(@(x) fieldnames(x), ss, 'un', 0);
+                    fs = reshape(vertcat(fs{:}), [], 1);
+                    fs = unique(fs);
+                    ss = obj.fill_empty(ss, fs);
+                    ss = vertcat(ss{:});
+                end
+                if any(ist) || nargin < 3 || ~preserveFormat
+                    if isstruct(ss)
+                        ss = struct2table(ss);
+                    end
                 end
             end
         end
         
-        function ss = fill_empty(obj, ss, fields, evals)
+        function ss = fill_empty(obj, ss, fields, emptvals)
             %% Fill absent values with empty
             if nargin < 4
-                evals = {-1, ''};
+                emptvals = {-1, ''};
             end
             ist = istable(ss);
             if ist
@@ -44,16 +53,16 @@ classdef Tables
                 fex = cellfun(@(x, f) isfield(x, f), ss, repmat(f, length(ss), 1));
                 if iscm
                     if fields(f{1}) == "double"
-                        e = evals{1};
+                        e = emptvals{1};
                     else
-                        e = evals{2};
+                        e = emptvals{2};
                     end
                 else
                     tc = cellfun(@(x, f) class(getfield(x, f)), ss(fex), repmat(f, length(ss(fex)), 1), 'un', 0);
                     if length(unique(tc)) == 1 && tc{1} == "double"
-                        e = evals{1};
+                        e = emptvals{1};
                     else
-                        e = evals{2};
+                        e = emptvals{2};
                     end
                 end
                 ss(~fex) = obj.addfield(ss(~fex), f{1}, e);
@@ -62,17 +71,51 @@ classdef Tables
                 ss = struct2table(ss{1}, 'AsArray', true);
             end
         end
+
+        function a = cell2double(~, c, fs)
+            %% Convert cell array of doubles to numeric array
+            ist = istable(c);
+            if ist
+                t = c;
+                c = t{:, fs};
+            end
+            c = cellfun(@double, c, 'un', 0);
+            ise = cellfun('isempty', c);
+            c(ise) = {NaN};
+            a = cell2mat(c);
+            if ist
+                fs = string(fs);
+                for i = 1 : length(fs)
+                    t.(fs(i)) = a(:, i);
+                end
+                a = t;
+            end
+        end
+        
+        function T = cm2table(obj, cm)
+            %% Convert containers.Map to table
+            k = keys(cm)';
+            v = values(cm)';
+            v = obj.concat(v);
+            T = table(k, v, 'VariableNames', {'Keys' 'Values'});
+        end
+        
+        function t = fillmissingfrom(~, t, var1, var2)
+            %% Fill empty values in var1 with values from var2
+            ise = ismissing(t{:, var1});
+            t{ise, var1} = t{ise, var2};
+        end
         
         function ss = addfield(~, ss, f, val)
-            %% add field to struct cell
+            %% Add field to struct cell
             isc = iscell(ss);
             ist = istable(ss);
             if ist, ss = table2struct(ss); end
             if ~isc, ss = {ss}; end
             if ~iscell(val), val = {val}; end
-            addfwrap = @(x) addf(x, f, val);
+            addfwrap = @(x) addfield_helper(x, f, val);
             ss = cellfun(@(x) addfwrap(x), ss, 'un', 0);
-            function x = addf(x, f, e)
+            function x = addfield_helper(x, f, e)
                 em = repmat(e, length(x), 1);
                 if iscell(em)
                     [x.(f)] = em{:};
@@ -96,15 +139,45 @@ classdef Tables
             if isfield(s, f)
                 vals = {s.(f)}';
                 isVal = ~cellfun('isempty', vals);
-                vals(isVal) = cellfun(@(c) c.(subf), vals(isVal), 'un', 0);
-                c = cellfun(@class, vals(isVal), 'un', 0);
+                hasField = false(size(isVal));
+                hasField(isVal) = cellfun(@(c) isfield(c, subf), vals(isVal));
+                vals(hasField) = cellfun(@(c) c.(subf), vals(hasField), 'un', 0);
+                c = cellfun(@class, vals(hasField), 'un', 0);
                 c = unique(c);
-                if (length(c) == 1) && (c == "double")
-                    vals(~isVal) = {-1};
+                if length(c) == 1
+                    if c == "double"
+                        emp = {-1};
+                    elseif c == "char"
+                        emp = {''};
+                    end
+                    vals(~hasField) = emp;
                 end
                 [s.(newf)] = vals{:};
             end
             if ist, s = struct2table(s, 'AsArray', true); end
+        end
+        
+        function t = expand(obj, t, f, preserve, append_names)
+            %% Expand table's structure variable to new variables
+            if nargin < 4
+                preserve = false;
+            end
+            if nargin < 5
+                append_names = true;
+            end
+            newt = obj.concat(t{:, f});
+            if isstruct(newt)
+                newt = struct2table(newt);
+            end
+            vnames = newt.Properties.VariableNames;
+            if append_names
+                vnames = f + "_" + vnames;
+            end
+            newt.Properties.VariableNames = vnames;
+            t = [t newt];
+            if ~preserve
+                t(:, f) = [];
+            end
         end
         
         function t = rmvars(~, t, vars)
@@ -131,6 +204,12 @@ classdef Tables
             %% Count occurences in vector
             vec = categorical(vec);
             t = table(categories(vec), countcats(vec), 'VariableNames', {vname, 'count'});
+        end
+        
+        function t = unique(~, t, vname)
+            %% Unique rows in table by vname
+            [~, ia] = unique(t{:, vname});
+            t = t(ia, :);
         end
     end
 end
