1d1c198fb3a23e50e8b53853c1ec1a667deed145
Pavel Roslovets <roslovets@exponenta.ru>
2019-10-10 19:26:24 +0300

    YouTube: improve search()

--- +WEB/YouTube.m
+++ +WEB/YouTube.m
@@ -17,7 +17,7 @@ classdef YouTube < WEB.API.Common
             obj.key = api_key;
         end
         
-        function [res, apiopts] = call_api(obj, method, params, vars)
+        function [res, err, apiopts] = call_api(obj, method, params, vars)
             %% Get via API
             [params, apiopts] = obj.prepare_params(params, vars);
             req = WEB.API.Req(obj.URL);
@@ -26,25 +26,35 @@ classdef YouTube < WEB.API.Common
             req.addquery('key', obj.key);
             req.addquery('part', 'snippet');
             req.setopts('Timeout', obj.timeout);
-            res = get(req);
+            [res, err] = get(req);
         end
         
-        function [T, res] = search(obj, varargin)
+        function [T, res, err] = search(obj, varargin)
             %% Get current weather
             method = 'search';
             params = {
                 'q', 'optional', ''
                 'channelId', 'optional', ''
                 'maxResults', 'optional', 5
+                'order', 'optional', 'relevance'
                 };
-            [res, apiopts] = obj.call_api(method, params, varargin);
-            T = [obj.TU.concat({res.items.snippet}) obj.TU.concat({res.items.id})];
-            T.publishedAt = datetime(T.publishedAt, 'InputFormat', "yyyy-MM-dd'T'HH:mm:ss'.000Z'");
-            if ~obj.TU.isvar(T, 'videoId')
-                T = obj.TU.addfield(T, 'videoId', '');
-            end
-            if ~obj.TU.isvar(T, 'playlistId')
-                T = obj.TU.addfield(T, 'playlistId', '');
+            [res, err, apiopts] = obj.call_api(method, params, varargin);
+            if ~isempty(res) && ~isempty(res.items)
+                info = obj.TU.concat({res.items.id});
+                if obj.TU.isvar(info, 'channelId')
+                    info = removevars(info, 'channelId');
+                end
+                T = [obj.TU.concat({res.items.snippet}) info];
+                T.publishedAt = datetime(T.publishedAt, 'InputFormat', "yyyy-MM-dd'T'HH:mm:ss'.000Z'");
+                if ~obj.TU.isvar(T, 'videoId')
+                    T = obj.TU.addfield(T, 'videoId', '');
+                    T = movevars(T, 'videoId', 'after', 'kind');
+                end
+                if ~obj.TU.isvar(T, 'playlistId')
+                    T = obj.TU.addfield(T, 'playlistId', '');
+                end
+            else
+                T = table();
             end
         end
         
